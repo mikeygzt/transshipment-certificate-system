4d3d47e49596d97a19715cc478009bb2f47b0cc6
Michael Palmer <michael.palmer@jca.gov.jm>
2026-07-24 11:13:36 -0500

    Initial project setup

---
+++ transshipment-api/src/main/java/jm/gov/jca/transshipment_api/TransshipmentApiApplication.java
@@ -0,0 +1,13 @@
+package jm.gov.jca.transshipment_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransshipmentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransshipmentApiApplication.class, args);
+	}
+
+}
