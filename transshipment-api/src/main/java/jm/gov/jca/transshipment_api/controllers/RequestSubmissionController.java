40161a3553a3cf314ae9b54c1dd14089fa5314e8
WatoDej <watsondejone5@gmail.com>
2026-07-28 08:35:09 -0500

    Currently largely empty controllers

---
+++ transshipment-api/src/main/java/jm/gov/jca/transshipment_api/controllers/RequestSubmissionController.java
@@ -0,0 +1,14 @@
+package jm.gov.jca.transshipment_api.controllers;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RequestSubmissionController{
+
+    @Autowired
+
+    @GetMapping("/request") 
+    public ResponseEntity<?>
+
+}
