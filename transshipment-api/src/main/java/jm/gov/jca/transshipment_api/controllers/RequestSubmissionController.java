9cb176fd05654e3f5000d9fc23243a1014af5e5d
WatoDej <watsondejone5@gmail.com>
2026-07-28 08:38:52 -0500

    temporary commented out controllers

--- transshipment-api/src/main/java/jm/gov/jca/transshipment_api/controllers/RequestSubmissionController.java
+++ transshipment-api/src/main/java/jm/gov/jca/transshipment_api/controllers/RequestSubmissionController.java
@@ -3,7 +3,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+/*@RestController
 public class RequestSubmissionController{
 
     @Autowired
@@ -11,4 +11,4 @@ public class RequestSubmissionController{
     @GetMapping("/request") 
     public ResponseEntity<?>
 
-}
+}*/
--- transshipment-api/src/main/java/jm/gov/jca/transshipment_api/controllers/RequesterLoginController.java
+++ transshipment-api/src/main/java/jm/gov/jca/transshipment_api/controllers/RequesterLoginController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+/*@RestController
 public class RequesterLoginController{
     //public RequesterLoginController(){}
 
@@ -25,4 +25,4 @@ public class RequesterLoginController{
 
 
 
-}
+}*/
